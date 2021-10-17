39fdd7d24ea42bc1ba596c9ef572cab9dea85200
Baluk Pavel <xpomocoma001@outlook.com>
2021-10-17 17:26:50 +0300

    fix

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -7,14 +7,13 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
-    static public Main S;
+    public static Main S;
     static Dictionary<WeaponType, WeaponDefinition> WEAP_DICT;
 
     [Header("Set in Inspector")]
     public GameObject[] prefabEnemies;
     public float enemySpawnPerSecond = 0.25f;
     public float enemyDefaultPadding = 1.5f;
-
     public WeaponDefinition[] weaponDefinitions;
     public GameObject prefabPowerUp;
     public WeaponType[] powerUpFrequency = new WeaponType[] {
@@ -76,25 +75,6 @@ public class Main : MonoBehaviour
         Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
     }
 
-/*    public void SpawnCheatEnemy() {
-
-        int ndx = Random.Range(0, prefabEnemies.Length);
-        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
-
-        float enemyPadding = enemyDefaultPadding;
-        if (go.GetComponent<BoundsCheck>() != null) {
-            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
-        }
-
-        Vector3 pos = Vector3.zero;
-        float xMin = -bndCheck.camWidth + enemyPadding;
-        float xMax = bndCheck.camWidth - enemyPadding;
-        pos.x = Random.Range(xMin, xMax);
-        pos.y = bndCheck.camHeigth + enemyPadding;
-        go.transform.position = pos;
-
-    }*/
-
     public void DelayedRestart(float delay) {
         Invoke("Restart", delay);
     }
@@ -102,21 +82,8 @@ public class Main : MonoBehaviour
     public void Restart() {
         SceneManager.LoadScene("_Scene_0");
     }
-/// <summary>
-/// —татическа€ функци€, возвращающа€ WeaponDefinition из статического
-/// защищенного пол€ WEAP_DICT класса Main.
-/// </summary>
-/// 
-/// <param name="wt">
-/// “ип оружи€ WeaponType, дл€ которого требуетс€ получить 
-/// WeaponDefinition.
-/// </param>
-/// 
-/// <returns>
-/// Ёкземпл€р WeaponDefinition или, если нет такого определени€ дл€ указанного
-/// WeaponType, возвращает новый экземпл€р WeaponDefinition с типом none.
-/// </returns>
-    static public WeaponDefinition GetWeaponDefinition(WeaponType wt) {
+
+    public static WeaponDefinition GetWeaponDefinition(WeaponType wt) {
         if (WEAP_DICT.ContainsKey(wt)) {
             return (WEAP_DICT[wt]);
         }
